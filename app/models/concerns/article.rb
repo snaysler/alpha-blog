bd3562f22cebbd0cc9fb05ded2829a323f3315d4
snaysler <tbrukila@uvm.edu>
2016-07-08 06:46:23 +0000

    Added some stylistic effects

--- app/models/concerns/article.rb
+++ app/models/concerns/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 20, maximum: 500}
+  validates :title, presence: true, length: {minimum: 1, maximum: 50}
+  validates :description, presence: true, length: {minimum: 20, maximum: 10000}
 end
